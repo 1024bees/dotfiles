e970003840742c9d1911b84abc67a1c514ed5b1c
James Connolly <jconnolly@tenstorrent.com>
2021-07-06 15:27:24 +0000

    update dotfiles

--- install.sh
+++ install.sh
@@ -8,4 +8,9 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 mv nvim.appimage nvim
 chmod +x nvim
 
+
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf\
+~/.fzf/install
+
 echo "Make sure that ~/.local/bin is in PATH!"

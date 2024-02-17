e302c76827ed70c49eab36fdd75a5dc849264eb2
James Connolly <james@Jamess-MacBook-Pro.local>
2024-02-17 13:52:06 -0500

    update to mac

--- install_nvim.sh
+++ install_nvim.sh
@@ -4,24 +4,23 @@ ln -s $(readlink -f nvim) ~/.config/
 
 mkdir -p ~/.local/bin/
 cd ~/.local/bin/
-wget https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage
-mv nvim.appimage nvim
-chmod +x nvim
-
-
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf\
-~/.fzf/install
-
-export RUSTUP_HOME=/localdev/${USER}/.rustup 
-export CARGO_HOME=/localdev/${USER}/.cargo
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-
-cargo install stylua
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-macos.tar.gz
+tar xzvf nvim-macos.tar.gz
+mv ./nvim-macos/bin/nvim ~/.local/bin
 
 
 
 
-
-
-echo "Make sure that ~/.local/bin is in PATH!"
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf\
+#~/.fzf/install
+#
+#curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+#
+#cargo install stylua
+#
+#
+#
+#
+#
+#
+#echo "Make sure that ~/.local/bin is in PATH!"
